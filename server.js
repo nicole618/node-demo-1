c792f19f99f6a4528ea7b1902c15aa0e33ee3b37
nicole <1251501326@qq.com>
2021-01-28 20:38:33 +0800

    彭于晏的小迷妹

--- server.js
+++ server.js
@@ -24,9 +24,16 @@ var server = http.createServer(function(request, response){
   if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`哈哈哈`)
+    response.write(`<!DOCTYPE html>
+    <head>
+        <link rel="stylesheet" href="/style">
+    </head>
+    <body>
+        <h1>彭于晏的小迷妹</h1>
+    </body>
+    `);
     response.end()
-  } else if(path === '/x'){
+  } else if(path === '/style'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
     response.write(`body{color: red;}`)
@@ -34,7 +41,7 @@ var server = http.createServer(function(request, response){
   } else {
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`你输入的路径不存在对应的内容`)
+    response.write(`你访问的页面不存在`)
     response.end()
   }
 
